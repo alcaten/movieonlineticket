d047b00fd48226aac0b313cb8ad6551617861d50
alcaten <107264535+alcaten@users.noreply.github.com>
2022-06-25 17:49:22 +0800

    Add files via upload

---
+++ movieticketsonline/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using movieticketsonline.Models;
+
+namespace movieticketsonline.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movie).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actors).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Actor_Movie> Actors_Movie { get; set; }
+
+    }
+}
---
+++ movieticketsonline/Data/Enums/MovieCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace movieticketsonline.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Fantasy,
+        Horror,
+        Mystery,
+        Romance,
+        SciFi,
+
+    }
+}
